1198fa5f5eb9c79b7474eb983f1193daebc89e22
walidrifi1 <157632199+walidrifi1@users.noreply.github.com>
2024-02-27 23:33:50 +0100

    Add files via upload

---
+++ 8 BCN - TGN/main.js
@@ -0,0 +1,32 @@
+function cienkm(){
+
+    document.querySelector("#inicial").src="/images/dubai.jpg";
+
+}
+
+function cincuentakm(){
+
+    document.querySelector("#inicial").src="/images/hawai.jpg";
+
+}
+
+function quincekm(){
+
+    document.querySelector("#inicial").src="/images/maldivas.jpg";
+
+
+}
+
+function cerokm(){
+
+    document.querySelector("#inicial").src="/images/destino.webp";
+
+
+}
+
+function reiniciar(){
+
+    document.querySelector("#inicial").src="images/inicio.jpg";
+
+
+}
